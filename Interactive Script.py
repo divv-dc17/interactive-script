7addcce2f0e08d33edef471f2fc58ddd427d1943
divv-dc17 <96064568+divv-dc17@users.noreply.github.com>
2021-12-17 20:16:16 +0530

    Add files via upload

---
+++ Interactive Script.py
@@ -0,0 +1,33 @@
+# Python Activity by Divyansh Chaturvedi of 8th-B
+# # Interactive Script in Python # #
+
+import webbrowser
+import time
+
+sentence = "Ohh...! You are " 
+name = str(input("Write your name. \n"))
+
+final = (sentence+name)
+print(final)
+
+time.sleep(1.5)
+word = str(input("But... Sorry, didn't recognize you. May I know your full name? \n"))
+
+if word == "Divyansh Chaturvedi":
+    time.sleep(2)
+    print ("Ohh... I got it now ! Welcome Back !")
+else:
+    time.sleep(2)
+    print ("Pardon me, let me see if I can do something for you.")
+
+time.sleep(3)
+Task = str (input("I can do a work for you. I can open the browser. Shall I? \n"))
+
+if Task == "Sure":
+    webbrowser.open("https://www.google.com")
+    time.sleep(5)
+    print("Have fun mate !")
+else:
+    print("Alright, as you wish.")
+
+# Here's the end. Hope you liked my project.
